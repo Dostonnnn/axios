4fb8b55d32c21da2cd23d62353eca9cb2cfa1cb6
Dostonnnn <davronovvv707@gmail.com>
2026-06-23 18:57:37 +0500

    done

---
+++ index.js
@@ -0,0 +1,31 @@
+const api = `https://dummyjson.com/products`;
+const getApi = async (url) => {
+  try {
+    const response = await axios.get(url).then((result) => {
+      console.log(result.data);
+      showProducts(result.data.products);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getApi(api);
+const cards = document.querySelector(".cards");
+cards.innerHTML = "Loading";
+function showProducts(result) {
+  cards.innerHTML = "";
+  result.forEach((element) => {
+    const { title, category, description, price } = element;
+    console.log();
+    cards.innerHTML += `
+        <div class="card">
+          <h4 class="card-title">${title}</h4>
+          <p class="category">${category}</p>
+          <p class="description">${description}</p>
+          <p class="price">${price}</p>
+        </div>
+
+    `;
+  });
+}
