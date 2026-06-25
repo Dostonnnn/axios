ee7b7aca1d1d1197a9ab0273e262e500a20ee780
Dostonnnn <davronovvv707@gmail.com>
2026-06-25 22:47:20 +0500

    search and mode

--- index.js
+++ index.js
@@ -1,34 +1,65 @@
-const api = `https://dummyjson.com/products`;
-const getApi = async (url) => {
-  try {
-    const response = await axios.get(url).then((result) => {
-      console.log(result.data);
-      showProducts(result.data.products);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+const api = `https://jsonplaceholder.typicode.com/posts`;
+let products = [];
 
-getApi(api);
+async function getApi(api) {
+  try {
+    const response = await fetch(api);
+    if (!response.ok) {
+      throw new Error("Same error occured");
+    }
+    const data = await response.json();
+    products = data;
+    showProducts(products);
+    console.log(products);
+  } catch (error) {
+    console.error(error);
+    cards.innerHTML = "Error loading data";
+  }
+}
 const cards = document.querySelector(".cards");
 const loading = "Loading";
 cards.innerHTML = loading;
+
+getApi(api);
+
 function showProducts(result) {
   cards.innerHTML = "";
+  let newTitle;
   result.forEach((element) => {
-    const { title, category, description, price } = element;
-    console.log();
+    const { title, userId, body, id } = element;
+    if (title.length > 20) {
+      newTitle = title.slice(0, 20) + "...";
+    }
     cards.innerHTML += `
         <div class="card">
-          <div class="image-box">
-            <img src="${element.images}" alt="${title}" class="image" />
-          </div>
-          <h4 class="card-title">${title}</h4>
-          <p class="category">${category}</p>
-          <p class="description">${description}</p>
-          <p class="price">${price}</p>
+          <h4 class="card-title">${newTitle}</h4>
+          <p class="category">${id}</p>
+          <p class="description">${body}</p>
         </div>
     `;
   });
 }
+
+const search = document.querySelector(".search");
+search.addEventListener("input", (e) => {
+  e.preventDefault();
+  const text = e.target.value.toLowerCase();
+  const filterData = products.filter((item) => {
+    let titleValue = item.title.toLowerCase().includes(text);
+    let idValue = item.id.toString().includes(text);
+    return titleValue || idValue;
+  });
+  console.log(text);
+  showProducts(filterData);
+});
+const body = document.querySelector("body");
+const mode = document.querySelector(".dark-mode");
+mode.addEventListener("click", (e) => {
+  e.preventDefault();
+  body.classList.toggle("dark-theme");
+  if (body.classList.contains("dark-theme")) {
+    mode.textContent = "☀️";
+  } else {
+    mode.textContent = "🌑";
+  }
+});
